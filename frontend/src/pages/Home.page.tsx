cc425965b7c9e9f3269f39be432c918fa1cd3bcc
ilyas <ibaraaouche@gmail.com>
2024-05-21 15:51:28 +0100

    ready to deploy

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -11,7 +11,8 @@ import { Navigate } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] tanslate-y-[-50]">
+        <div className="absolute top-[20%] left-[50%] translate-x-[-50%] tanslate-y-[-50]">
+            <img className="mb-8" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dollar_sign_in_circle_cleaned_%28PD_version%29.svg/1200px-Dollar_sign_in_circle_cleaned_%28PD_version%29.svg.png" width={200} alt=""/>
             <SignedOut>
                 <h1 className="font-mono font-black text-2xl"> Welcome to Your Own Personal Finance Tracker!</h1>
                 <SignUpButton mode="modal"/>
